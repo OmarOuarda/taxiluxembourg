9534c9eb419c2212d028e8cfe9d33d9848e881ba
OmarOuarda <110228790+OmarOuarda@users.noreply.github.com>
2023-11-08 10:27:23 +0100

    Add files via upload

---
+++ thankyou.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html> 
+
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="stylesheet" href="css/animated.css">
+    </head>
+<body translate="no">
+
+  <div class="heart">
+  ❤
+  
+</div>
+<h5 style="text-transform:uppercase">Your booking has been sent, <br> wait for our call in a minute.</h5>
+<div class="text">
+    
+  <p>
+    T
+  </p>
+  <p>
+    H
+  </p>
+  <p>
+    A
+  </p>
+  <p>
+    N
+  </p>
+  <p>
+    K
+  </p>
+  <p>
+    &nbsp;
+  </p>
+  <p>
+    Y
+  </p>
+  <p>
+    0
+  </p>
+  <p>
+    U
+  </p>
+</div>
+  
+  
+  
+
+
+
+</body>
+
+<?php 
+header("Refresh:5;url=index.php");
+
+?>
+
+</html>
